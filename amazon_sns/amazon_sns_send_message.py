f8b2d5a2bc4f62976eec9658c409bbb5aa21ecfb
Alfredo Sanchez <alfre32@gmail.com>
2018-03-02 17:18:38 +0100

    AWS credentials settings

--- amazon_sns/amazon_sns_send_message.py
+++ amazon_sns/amazon_sns_send_message.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 import boto3
 
+# AWS Credentials
+AWS_ACCESS_KEY_ID = "YOUR ACCES KEY"
+AWS_SECRET_ACCESS_KEY = "YOUR SECRET KEY"
+AWS_REGION_NAME = "eu-west-1"
+
 # Create an SNS client
 client = boto3.client(
     "sns",
-    aws_access_key_id="YOUR ACCES KEY",
-    aws_secret_access_key="YOUR SECRET KEY",
-    region_name="us-east-1"
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=AWS_REGION_NAME
 )
 
 # Send your sms message.
--- amazon_sns/amazon_sns_send_multiple_message.py
+++ amazon_sns/amazon_sns_send_multiple_message.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 import boto3
 
+# AWS Credentials
+AWS_ACCESS_KEY_ID = "YOUR ACCES KEY"
+AWS_SECRET_ACCESS_KEY = "YOUR SECRET KEY"
+AWS_REGION_NAME = "eu-west-1"
+
 # Create an SNS client
 client = boto3.client(
     "sns",
-    aws_access_key_id="YOUR ACCES KEY",
-    aws_secret_access_key="YOUR SECRET KEY",
-    region_name="us-east-1"
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=AWS_REGION_NAME
 )
 
 # Create the topic if it doesn't exist (this is idempotent)
@@ -16,8 +21,8 @@ topic_arn = topic['TopicArn']  # get its Amazon Resource Name
 some_list_of_contacts = [
     '+12223334444',
     '+12223334445',
-    '+12223334446'
-}
+    '+12223334446',
+]
 
 # Add SMS Subscribers
 for number in some_list_of_contacts:
